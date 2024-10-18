abc2cb739f6c6f32a64117b248fc5178dd63bc25
unknown <BT_3W2_19@BKTECH.LOCAL>
2024-10-18 15:30:21 -0400

    finished-part-C-started-part-E

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 // Counter App
+saved_counts = []
 
 clicks = 0
+clickPower = 1
+powerUpgradeCost = 10
+
+powerCostDisplay = document.getElementById("increment-power-cost").innerHTML
+powerCostDisplay.innerHTML = powerUpgradeCost
+
+
 
 function increment() {
     clicks++
     document.getElementById("count-el").innerText = clicks
+}
+
+function upgradeClickPower() {
+    if (clicks >= powerUpgradeCost) {
+        clicks -= powerUpgradeCost
+        clickPower++
+        powerUpgradeCost = Math.floor(powerUpgradeCost * 1.1)
+    }
+}
+
+function save() {
+    saved_counts.push(clicks)
+    clicks = 0
+    document.getElementById("count-el").innerText = clicks
+    document.getElementById("saved-counts").innerText = saved_counts
 }
